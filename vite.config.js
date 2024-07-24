1a5323cf55887f3cb52dd9dc28bba96a9dfb0758
Onos <onosejoor14@gmail.com>
2024-07-24 21:42:22 +0100

    Add Keeper App

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://github.com/onosejoor/Keeper-App.git",
   plugins: [react()],
   server:{
     port: 3000,
